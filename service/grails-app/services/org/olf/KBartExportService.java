bcc55b8b986b1077dbf125de38d0463a00bbf053
aclseelen <alfons.seelen@hbz-nrw.de>
2019-01-24 14:34:43 +0100

    MODERM-18: add service and test template (java) for KBartExport

---
+++ service/grails-app/services/org/olf/KBartExportService.java
@@ -0,0 +1,13 @@
+package org.olf;
+
+import grails.gorm.services.Service;
+import grails.gorm.transactions.Transactional;
+
+@Transactional
+@Service
+public class KBartExportService {
+
+  public void getActiveTitles() {
+
+  }
+}
