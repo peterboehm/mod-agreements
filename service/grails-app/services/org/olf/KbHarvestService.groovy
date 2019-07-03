6f971197a59ee1f14a32728a4fde60dcd6469a4c
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2019-07-03 09:26:18 -0400

    Restore standard sync behavior

--- service/grails-app/services/org/olf/KbHarvestService.groovy
+++ service/grails-app/services/org/olf/KbHarvestService.groovy
@@ -48,8 +48,7 @@ class KbHarvestService {
     Tenants.withId(schemaName, this.&triggerCacheUpdate) 
   }
 
-  //@Scheduled(fixedDelay = 3600000L, initialDelay = 3600000L) // Run task every hour
-  @Scheduled(fixedDelay = 300000L, initialDelay = 20000L) // Run every 5 mins
+  @Scheduled(fixedDelay = 3600000L, initialDelay = 3600000L) // Run task every hour
   void triggerSync() {
     log.debug "Running scheduled KB sync for all tenants :{}", new SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(new Date())
 
