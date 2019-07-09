12cab34b487c4f5203a35db0329c9574246850ae
Claudia Malzer <malzer@ulb.tu-darmstadt.de>
2019-07-09 17:47:22 +0200

    ERM-291: Add usage data provider to subscription agreement
    change UsageDataProvider domain class

--- service/grails-app/domain/org/olf/erm/UsageDataProvider.groovy
+++ service/grails-app/domain/org/olf/erm/UsageDataProvider.groovy
@@ -7,25 +7,19 @@ import com.k_int.web.toolkit.refdata.RefdataValue
 import grails.gorm.MultiTenant
 
 public class UsageDataProvider extends RemoteOkapiLink implements MultiTenant<UsageDataProvider> {
-	String id 
-	String version
-	String usageDataProviderId
+
 	String usageDataProviderNote
   
 	static belongsTo = [ owner: SubscriptionAgreement ]
   
 	static mapping = {
-	                 id column: 'udp_id', generator: 'uuid2', length:36
-	            version column: 'udp_version'
 	              owner column: 'udp_owner_fk'
-    usageDataProviderId column: 'udp_remote_id'
   usageDataProviderNote column: 'udp_note', type: 'text'
                   
 	}
   
 	static constraints = {
 		owner(nullable:false, blank:false)
-		usageDataProviderId(nullable:true, blank:false)
         usageDataProviderNote(nullable:true, blank:false)
 	}
 
--- service/grails-app/migrations/usage-data-provider.groovy
+++ service/grails-app/migrations/usage-data-provider.groovy
@@ -1,13 +1,13 @@
 databaseChangeLog = {  
 	changeSet(author: "claudia (manual)", id: "2019-07-09-00001") {
 		createTable(tableName: "usage_data_provider") {
-			column(name: "udp_id", type: "VARCHAR(36)") {
+			column(name: "rol_id", type: "VARCHAR(36)") {
 				constraints(nullable: "false")
 			}
-			column(name: "udp_version", type: "BIGINT") {
+			column(name: "rol_version", type: "BIGINT") {
 				constraints(nullable: "false")
 			}
-			column(name: "udp_remote_id", type: "VARCHAR(36)") {
+			column(name: "rol_remote_id", type: "VARCHAR(50)") {
 				constraints(nullable: "false")
 		    }
 			column(name: "udp_owner_fk", type: "VARCHAR(36)") {
@@ -18,7 +18,7 @@ databaseChangeLog = {
 	  }
 	  
 	  changeSet(author: "claudia (manual)", id: "2019-07-09-00002") {
-		  addPrimaryKey(columnNames: "udp_id", constraintName: "usage_data_providerPK", tableName: "usage_data_provider")
+		  addPrimaryKey(columnNames: "rol_id", constraintName: "usage_data_providerPK", tableName: "usage_data_provider")
 	  }
 	  
 	  changeSet(author: "claudia (manual)", id: "2019-07-09-00003") {
